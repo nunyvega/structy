e15666997dd9ba7221f761e8efcc7ff245712a7a
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-06 13:48:21 +0200

    Add compress.js to exo solutions

---
+++ anagrams.js
@@ -0,0 +1,15 @@
+const anagrams = (s1, s2) => {
+    s1 = s1.split("").sort()
+    s2 = s2.split("").sort()
+    
+    if (JSON.stringify(s1) == JSON.stringify(s2)){
+      return true
+    } 
+      return false;
+  
+  };
+  
+  module.exports = {
+    anagrams,
+  };
+  
