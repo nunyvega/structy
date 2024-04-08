084bbe41ec292d0c70b4fd21ea49a1ce4ad89297
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-08 16:01:15 +0200

    five sort arr

---
+++ ArrayAndString/five-sort.js
@@ -0,0 +1,37 @@
+const fiveSort = (nums) => {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right){
+      if (nums[right]=== 5){
+        right--;
+      } else {
+        if (nums[left] === 5 ) [nums[left], nums[right]] = [nums[right], nums[left]]
+        left++
+      }
+    }
+    return nums
+  };
+  
+  /*
+  test_00 [PASS] 64ms 
+  test_01 [PASS] 61ms 
+  test_02 [PASS] 65ms 
+  test_03 [PASS] 63ms 
+  test_04 [PASS] 58ms 
+  test_05 [PASS] 921ms 
+  const fiveSort = (nums) => {
+  
+    for (let x = nums.length - 1; x >= 0; x--){
+      if (nums[x] == 5){
+        nums.splice(x,1);
+        nums.push(5);
+      }
+    }
+    return nums
+  };
+  */
+  module.exports = {
+    fiveSort,
+  };
+  
