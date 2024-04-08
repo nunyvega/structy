605dac8473528fe4baa094291521efeccab24007
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-08 16:00:44 +0200

    intersection exo

---
+++ ArrayAndString/intersection.js
@@ -0,0 +1,14 @@
+const intersection = (a, b) => {
+    // todo
+    const setA = new Set(a);
+    const intersect = [];
+    for (const num of b) if (setA.has(num)) intersect.push(num)
+  
+    return intersect;
+    
+  };
+  
+  module.exports = {
+    intersection,
+  };
+  
