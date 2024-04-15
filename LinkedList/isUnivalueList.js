2c178ea6ec6527c34a8244937bc4ae1636d2cf3f
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-15 15:29:06 +0200

    add new exos, fix file extensions

---
+++ LinkedList/isUnivalueList.js
@@ -0,0 +1,32 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+
+// Recursive
+const isUnivalueList = (head) => {
+    if (head.next === null) return true;
+    if (head.val === head.next.val){
+      return isUnivalueList(head.next)
+    }
+    return false;
+  }
+  
+  /* iterative
+  const isUnivalueList = (head) => {
+    // todo
+    let value = head.val;
+    while (head !== null) {
+      if (head.val != value) return false;
+      head = head.next;
+    }
+    return true;
+  };
+  */
+  module.exports = {
+    isUnivalueList,
+  };
+  
