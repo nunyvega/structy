cc33d1fff8f20d21917f7f40b69bc2871439f602
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-16 15:48:12 +0200

    Depth first values / binary tree

---
+++ BinaryTree/DepthFirstValues.js
@@ -0,0 +1,45 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// Recursive
+const depthFirstValues = (root) => {
+  if (root === null) return []
+
+  const leftValues = depthFirstValues(root.left);
+  const rightValues = depthFirstValues(root.right);
+
+  return [root.val, ...leftValues, ...rightValues]
+}
+
+
+// Iterative
+const depthFirstValues2 = (root) => {
+  // todo
+  if (root === null) return []
+  const stack = [root];
+  const values = [];
+
+  while (stack.length > 0){
+    const current = stack.pop()
+    values.push(current.val)
+    
+    if (current.right){
+      stack.push(current.right)
+    }
+    
+    if (current.left){
+      stack.push(current.left)
+    }
+  }
+  return values;
+  
+};
+
+module.exports = {
+  depthFirstValues,
+};
