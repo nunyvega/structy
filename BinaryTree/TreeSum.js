01febba410ce82a19d79e6f5c01bd8ecec9302a7
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-16 15:59:56 +0200

    tree sum, tree includes solutions

---
+++ BinaryTree/TreeSum.js
@@ -0,0 +1,43 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// Recursive (depth first)
+const treeSum = (root) => {
+    if ( root === null) return 0
+  
+    const leftSum = treeSum(root.left)
+    const rightSum = treeSum(root.right)
+  
+    return root.val + leftSum + rightSum
+  }
+  
+  // Iterative (breadth first, using queue)
+  const treeSum2 = (root) => {
+    if ( root === null) return 0
+    const queue = [root]
+    let sum = 0;
+  
+    while (queue.length > 0){
+      const current = queue.shift()
+      sum += current.val
+  
+      if (current.left){
+        queue.push(current.left)
+      }
+  
+      if (current.right){
+        queue.push(current.right)
+      }
+    }
+    return sum;
+  };
+  
+  module.exports = {
+    treeSum,
+  };
+  
