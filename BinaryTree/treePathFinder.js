10d95b8e680ed4d87ed36de166577ed412f875fc
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-22 15:35:48 +0200

    Tree path finder solved

---
+++ BinaryTree/treePathFinder.js
@@ -0,0 +1,30 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const pathFinder = (root, target) => {
+    // todo
+    if (root == null) return null
+    if ( root.val === target) return [root.val]
+    if ( root.left === null && root.right === null) return null
+    let left = pathFinder(root.left, target)
+    let right = pathFinder(root.right, target)
+    if (Array.isArray(left)){
+      left.unshift(root.val)
+      return left
+    }
+    if (Array.isArray(right)){
+      right.unshift(root.val)
+      return right
+    }
+    return null
+  };
+  
+  module.exports = {
+    pathFinder,
+  };
+  
