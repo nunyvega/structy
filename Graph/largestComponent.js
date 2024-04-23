e4e48bb7ebdca86a233b54277213fe5566996081
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-23 15:20:32 +0200

    Largest component

---
+++ Graph/largestComponent.js
@@ -0,0 +1,29 @@
+const largestComponent = (graph) => {
+    // todo
+    const visited = new Set();
+    let largest = 0;
+    
+    for (node in graph){
+      let count = exploreAndCount(graph, node, visited);
+      if (count > largest) largest = count;
+    }
+  
+    return largest;
+  };
+  
+  const exploreAndCount = (graph, node, visited) => {
+    if (visited.has(String(node))) return 0;
+    visited.add(String(node));
+    
+    let count = 1;
+    for (neighbour of graph[node]){
+      count += exploreAndCount(graph, neighbour, visited)
+    }
+  
+    return count;
+  }
+  
+  module.exports = {
+    largestComponent,
+  };
+  
