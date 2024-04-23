3e833411d69aa49a908b56037a4aa3231122d582
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-04-23 15:18:59 +0200

    hasPath, first graph problem

---
+++ Graph/hasPath.js
@@ -0,0 +1,49 @@
+
+
+// Recursive depth first;
+const hasPath = (graph, src, dst) => {
+    if (src == dst) return true;
+    for (let neighbour of graph[src]){
+      if (hasPath(graph, neighbour, dst) === true) {
+        return true
+      }
+    }
+    return false;
+  }
+  
+  // Iterative breadth first;
+  const hasPath2 = (graph, src, dst) => {
+    const queue = [src];
+    
+    while (queue.length > 0){
+      const current = queue.shift();
+      if (current == dst) return true;
+  
+      for (let neighbour of graph[current]){
+        queue.push(neighbour)
+      }
+    }
+  
+    return false;
+  };
+  
+  // Iterative depth first;
+  const hasPath3 = (graph, src, dst) => {
+    const stack = [src];
+    
+    while (stack.length > 0){
+      const current = stack.pop();
+      if (current == dst) return true;
+  
+      for (let neighbour of graph[current]){
+        stack.push(neighbour)
+      }
+    }
+  
+    return false;
+  };
+  
+  module.exports = {
+    hasPath,
+  };
+  
