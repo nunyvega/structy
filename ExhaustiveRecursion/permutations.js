22e03fc32e6fc7a9af6d5c83b54e6cfea1931bee
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-05-03 12:37:45 +0200

    permutations revisit

--- ExhaustiveRecursion/permutations.js
+++ ExhaustiveRecursion/permutations.js
@@ -1,17 +1,19 @@
 const permutations = (items) => {
-	if (items.length === 0) return [[]];
-	const first = items[0];
-	const perms = permutations(items.slice(1));
+	if (items.length === 0) return [[]]; //base case
 
-	const fullPermutations = [];
+	const first = items[0]; // remove first item to then add it in different indexes
+	const perms = permutations(items.slice(1)); // recursively call w/out first
+
+	const allPermutations = []; // create array for responses
 	for (let perm of perms) {
+		// iterate over each permutation without first item
 		for (let i = 0; i <= perm.length; i++) {
-			let newArr = [...perm.slice(0, i), first, ...perm.slice(i)];
-			fullPermutations.push(newArr);
+			// Push first item in all possible indexes, including last (<= perm.length)
+			allPermutations.push([...perm.slice(0, i), first, ...perm.slice(i)]);
 		}
 	}
 
-	return fullPermutations;
+	return allPermutations;
 };
 
 //Perso approach
