f8b67273fcf54095fb3f10f73d4f37217004d731
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-05-03 12:32:53 +0200

    Revisit subsets

--- ExhaustiveRecursion/subsets.js
+++ ExhaustiveRecursion/subsets.js
@@ -1,29 +1,44 @@
 const subsets = (elements) => {
-	if (elements.length === 0) return [[]];
-
-	const first = elements[0];
+	if (elements.length === 0) return [[]]; // base case, no queda nada? arr vacio
+  
+   const firstElem = elements[0];
 	const allTheRest = elements.slice(1);
-	const without = subsets(allTheRest);
-	const withFirst = without.map((sub) => [first, ...sub]);
-
-	return [...without, ...withFirst];
-};
-
-//Perso
-const subsets2 = (elements, subsetsArray = [[]], memo = {}) => {
-	if (elements.length === 0) {
-		return subsetsArray;
-	}
+	const withoutFirst = subsets(allTheRest);
+	const withFirst = withoutFirst.map(arr => [firstElem, ...arr])
+  
+	return [...withoutFirst, ...withFirst]
+  
+  }/* How it works
+  [a,b] => 
+		  without First => b 
+							=> without First []
+							=> with first [b]
+							[[], [b]]
+		  with first =>  [[a], [a,b] ]
+	returns [[], [b], [a], [a,b]]
+	*/
+	
+  
+  const subsets2 = (elements, subsetsArray = [[]], memo = {}) => {
+	if (elements.length === 0) return subsetsArray;
 	const str = String(elements);
 	if (str in memo) return subsetsArray;
-
 	memo[str] = subsetsArray;
+	
 	subsetsArray.push(elements);
 	for (let x = 0; x < elements.length; x++) {
-		const newArr = [...elements];
-		newArr.splice(x, 1);
-		subsets(newArr, subsetsArray, memo);
+	  const newArr = [...elements];
+	  newArr.splice(x,1);
+	  subsets(newArr, subsetsArray, memo);
 	}
-
-	return memo[str];
-};
+  
+	return   memo[str] ;
+  };
+  
+  
+  
+  
+  module.exports = {
+	subsets,
+  };
+  
