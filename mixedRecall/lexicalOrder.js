8f04876d56ec232d8a161c0f2c6ac0f07aef6e27
nunyvega <16329583+nunyvega@users.noreply.github.com>
2024-07-25 11:46:26 +0200

    format

--- mixedRecall/lexicalOrder.js
+++ mixedRecall/lexicalOrder.js
@@ -1,24 +1,23 @@
 const lexicalOrder = (word1, word2, alphabet) => {
-    const alphabetIndex = {};
-    
-    alphabet.split("").forEach((value, index) => {
-       if (! alphabetIndex.hasOwnProperty(value)) {
-         alphabetIndex[value] = index;
-       }
-    })
-  
-    let iter = 0;
-    while (iter < word1.length){
-      if (word2[iter] === undefined) return false;
-      if (word1[iter] !== word2[iter]) {
-        return alphabetIndex[word1[iter]] < alphabetIndex[word2[iter]]
-      }
-      iter++;
+  const alphabetIndex = {};
+
+  alphabet.split("").forEach((value, index) => {
+    if (!alphabetIndex.hasOwnProperty(value)) {
+      alphabetIndex[value] = index;
     }
-    return true;
-  };
-  
-  module.exports = {
-    lexicalOrder,
-  };
-  
+  });
+
+  let iter = 0;
+  while (iter < word1.length) {
+    if (word2[iter] === undefined) return false;
+    if (word1[iter] !== word2[iter]) {
+      return alphabetIndex[word1[iter]] < alphabetIndex[word2[iter]];
+    }
+    iter++;
+  }
+  return true;
+};
+
+module.exports = {
+  lexicalOrder,
+};
